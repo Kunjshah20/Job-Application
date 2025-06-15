8e2d44207f50371c2864675de3d4977c490634f1
kunjshah4u <kunj08784@gmail.com>
2025-06-15 16:51:29 +0530

    1. Create delete jobs by company seq at job repository layer

--- src/main/java/com/jobx/firstjobpp/job/repositories/JobRepository.java
+++ src/main/java/com/jobx/firstjobpp/job/repositories/JobRepository.java
@@ -1,8 +1,15 @@
 package com.jobx.firstjobpp.job.repositories;
 
 import com.jobx.firstjobpp.job.dataobjects.Job;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JpaRepository<class name, primary key data type>
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    @Transactional
+    // Transactional makes sure all DB changes here happen together as one unit.
+    // If something goes wrong, it rolls everything back automatically.
+    // And delete by companySeq method implementation is handled by spring boot
+    void deleteByCompany_CompanySeq(Long companySeq);
 }
